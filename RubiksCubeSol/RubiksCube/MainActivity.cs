095da6a768fc9773e3c7c1f1efa73f22e4b3acdd
Alrineus Aldore <natanel475@gmail.com>
2022-06-07 02:32:18 +0300

    added main layout

--- RubiksCubeSol/RubiksCube/MainActivity.cs
+++ RubiksCubeSol/RubiksCube/MainActivity.cs
@@ -3,13 +3,16 @@ using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
 using Android.Widget;
+using Android.Views;
 
 namespace RubiksCube
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : AppCompatActivity, View.IOnClickListener
     {
-        TextView tv;
+        TextView tvHello;
+        Button btnPlay, btnRules, btnSaves;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -17,9 +20,14 @@ namespace RubiksCube
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
-            tv = FindViewById<TextView>(Resource.Id.tv);
+            tvHello = FindViewById<TextView>(Resource.Id.tvHello);
+            btnPlay = FindViewById<Button>(Resource.Id.btnPlay);
+            btnRules = FindViewById<Button>(Resource.Id.btnRules);
+            btnSaves = FindViewById<Button>(Resource.Id.btnSaves);
 
-            HandleCube();
+            btnPlay.SetOnClickListener(this);
+            btnRules.SetOnClickListener(this);
+            btnSaves.SetOnClickListener(this);
         }
 
 
@@ -29,7 +37,20 @@ namespace RubiksCube
 
             string cubeStr = cube.ToPrettyString();
 
-            tv.Text = cubeStr;
+            tvHello.Text = cubeStr;
+        }
+
+        public void OnClick(View v)
+        {
+            if (v == btnPlay)
+                HandleCube();
+            else if (v == btnRules)
+            {
+                if (btnSaves.Visibility == ViewStates.Visible)
+                    btnSaves.Visibility = ViewStates.Invisible;
+                else
+                    btnSaves.Visibility = ViewStates.Visible;
+            }
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
