352c8a18c44f06b32c5f95d9bd09e46484482690
Alrineus Aldore <natanel475@gmail.com>
2022-06-14 07:39:15 +0300

    deleted state from sql too

--- RubiksCubeSol/RubiksCube/StatesActivity.cs
+++ RubiksCubeSol/RubiksCube/StatesActivity.cs
@@ -12,14 +12,14 @@ using System.Text;
 namespace RubiksCube
 {
     [Activity(Label = "StatesActivity")]
-    public class StatesActivity : Activity, ListView.IOnItemClickListener
+    public class StatesActivity : Activity, ListView.IOnItemClickListener, ListView.IOnItemLongClickListener
     {
         public static List<State> statesList { get; set; }
         StateAdapter stateAdapter;
         ListView lvStates;
         string username;
 
-        Dialog d;
+        int lastPos; //used for alert dialog
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -38,11 +38,49 @@ namespace RubiksCube
             lvStates = FindViewById<ListView>(Resource.Id.lvStates);
             lvStates.Adapter = stateAdapter; //adds the adapter to the listview
             lvStates.OnItemClickListener = this;
+            lvStates.OnItemLongClickListener = this;
         }
 
         public void OnItemClick(AdapterView parent, View view, int position, long id)
         {
-            return;
+            //On item click, get cube str of state and initialize rubiks cube with it
+            Intent intent = new Intent(this, typeof(RubiksCubeActivity));
+            intent.PutExtra("cubeStr", statesList[position].cubeStr);
+            StartActivity(intent);
+        }
+
+        public bool OnItemLongClick(AdapterView parent, View view, int position, long id)
+        {
+            lastPos = position;
+            CreateAlertDialog();
+            return true;
+        }
+
+        private void CreateAlertDialog()
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.SetTitle("Wanna delete?");
+            builder.SetMessage("Sure you wanna Delete State");
+            builder.SetCancelable(true);
+
+            builder.SetPositiveButton("Delete State", YesAction);
+            builder.SetNegativeButton("Nevermind", NoAction);
+
+            AlertDialog dialog = builder.Create();
+            dialog.Show();
+        }
+
+        //Must functions for AlertDialog
+        public void YesAction(object sender, DialogClickEventArgs e)
+        {
+            //Delete item and update adapter
+            SQLiteHandler.Instance.DeleteState(statesList[lastPos]);
+            statesList.RemoveAt(lastPos);
+            stateAdapter.NotifyDataSetChanged();
+            Toast.MakeText(this, "Deleted state", ToastLength.Short).Show();
+        }
+        public void NoAction(object sender, DialogClickEventArgs e)
+        {
         }
 
         //update adapter after changes
